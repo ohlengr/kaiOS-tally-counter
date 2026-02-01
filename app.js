ac0b4185809b29ac654b540d5c52ee81033b8f97
ohlengr <ohlengr@gmail.com>
2026-02-01 22:40:22 +0530

    counter working enabled

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 document.addEventListener("DOMContentLoaded",function(){
-  var currentUrl = window.location.href;
-  console.log(window.location);
-  console.log(currentUrl);
-  if(currentUrl.pathname=="index.html"){
+  var pathname = window.location.pathname;
+  if(pathname.includes("index.html")){
     setTimeout(()=>{
       window.location.href = "counter.html";
     },1000);
+  }else if(pathname.includes("counter.html")){
+    var btn = document.getElementById("action-btn");
+    var digit = document.getElementById("digit-text");
+    var reset = document.getElementById("reset-btn");
+    var counter = 0;
+    if(!btn){
+      return;
+    }
+    btn.addEventListener('click', function(){
+      counter++;
+      if(counter<=99999){
+        digit.innerHTML = counter;
+      }
+    });
+    reset.addEventListener('click', function(){
+      window.location.reload();
+    });
   }
 });
